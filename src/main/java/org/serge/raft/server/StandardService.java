9d35bb1e5f0ec9fa6c2b4e1af7b8437f6bb4ad06
yangshj <754179755@qq.com>
2017-03-08 18:25:31 +0800

    标准服务实现类

---
+++ src/main/java/org/serge/raft/server/StandardService.java
@@ -0,0 +1,31 @@
+package org.serge.raft.server;
+
+
+public class StandardService implements Service, Lifecycle{
+
+	public Server getServer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setServer(Server server) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void start() throws LifecycleException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void stop() throws LifecycleException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
