678b2e941d51b59e61397ad698ff643fbd59aa45
yangshj <754179755@qq.com>
2017-03-08 18:24:58 +0800

    生命周期

---
+++ src/main/java/org/serge/raft/server/Lifecycle.java
@@ -0,0 +1,10 @@
+package org.serge.raft.server;
+
+public interface Lifecycle {
+	
+	/** 启动服务 */
+	public void start() throws LifecycleException;
+	
+	/** 停止服务 */
+	public void stop() throws LifecycleException;
+}
---
+++ src/main/java/org/serge/raft/server/LifecycleException.java
@@ -0,0 +1,53 @@
+package org.serge.raft.server;
+
+public class LifecycleException extends Exception {
+	
+	protected String message = null;
+	
+	protected Throwable throwable = null;
+	
+	
+	/////////////////////////////////////////////
+	
+	public LifecycleException() {
+		this(null, null);
+	}
+
+	public LifecycleException(String message) {
+		this(message, null);
+	}
+
+	public LifecycleException(Throwable throwable) {
+		this(null, throwable);
+	}
+
+	public LifecycleException(String message, Throwable throwable) {
+		super();
+		this.message = message;
+		this.throwable = throwable;
+	}
+	
+	
+	
+	/////////////////////////////////////////////////////////
+	
+	public String getMessage() {
+		return (message);
+	}
+	public Throwable getThrowable() {
+		return (throwable);
+	}
+	public String toString() {
+		StringBuffer sb = new StringBuffer("LifecycleException:  ");
+		if (message != null) {
+			sb.append(message);
+			if (throwable != null) {
+				sb.append(":  ");
+			}
+		}
+		if (throwable != null) {
+			sb.append(throwable.toString());
+		}
+		return (sb.toString());
+	}
+}
